3859c7ca516340959390768d95617903fb42b94e
Jian Wei Ng <s10208269@connect.np.edu.sg>
2023-02-17 03:19:49 +0800

    test

--- tests/integration-tests/test_matchStudent.py
+++ tests/integration-tests/test_matchStudent.py
@@ -1,7 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import time
 import sys, os, inspect
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -21,7 +21,11 @@ companydata_file_path = os.path.join(parentdir, companydata_filename)
 
 # in upload data page, upload a student csv file
 def test_upload_Data():
-    chrome_driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()), options=chrome_options)
     chrome_driver.get('http://127.0.0.1:5221/upload_data')
 
     chrome_driver.find_element(By.NAME, "internship_student_data").send_keys(studentdata_file_path)
